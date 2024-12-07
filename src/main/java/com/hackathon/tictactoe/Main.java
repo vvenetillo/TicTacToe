fcc8d6410b11718fde2a2648124add47cbce2424
vvenetillo <venetillovictor@gmail.com>
2024-12-07 15:42:13 -0300

    teste: teste de integração

--- src/main/java/com/hackathon/tictactoe/Main.java
+++ src/main/java/com/hackathon/tictactoe/Main.java
@@ -1,7 +1,10 @@
 package com.hackathon.tictactoe;
 
+import net.jorgedev.*;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Teste");
+        ConsoleClear.run();
         System.out.println("Hello World!");
     }
-}
+} 
