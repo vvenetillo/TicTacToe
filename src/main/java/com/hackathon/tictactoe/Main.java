349b52bf021662bc69ff9b7d8f101962803ece8e
Jorgeluisreis <ctt.jluis@gmail.com>
2024-12-07 16:39:46 -0300

    fix: Removido uma linha de teste

--- src/main/java/com/hackathon/tictactoe/Main.java
+++ src/main/java/com/hackathon/tictactoe/Main.java
@@ -1,12 +1,10 @@
 package com.hackathon.tictactoe;
 
 import com.hackathon.tictactoe.ui.ConsoleUI;
-
 import net.jorgedev.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste");
         ConsoleClear.run();
         System.out.println("Hello World!");
         ConsoleUI.exibirMenu();
