1a9cd636738b7ea7d8bd9ee57a83cf8927153c86
Gilvan <gilvanribeiro773@gmail.com>
2024-12-08 13:57:09 -0300

    exibirVitoriaJogador

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -1,5 +1,15 @@
 package com.hackathon.tictactoe.game;
 
 public class TicTacToeGame {
-
+    
+      public  static void exibirVitoriaUsuario() {
+         System.out.println("O jogador venceu!"); 
+        
+          System.out.println(" _____");
+         System.out.println(" | ^ ^ |"); 
+            System.out.println(" / \\");
+         System.out.println(" \\ U /"); 
+          System.out.println(" |||||"); 
+         
+           }
 }
