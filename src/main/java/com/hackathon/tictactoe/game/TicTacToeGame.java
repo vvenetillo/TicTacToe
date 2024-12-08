72b5742ec9795872c42d69a4bc199282ec0399e7
Arthur <arthur.ramalho.freire@gmail.com>
2024-12-08 12:42:05 -0300

    feat(TicTacToeGame):criado o método na classe TicTacToeGame

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -1,5 +1,28 @@
 package com.hackathon.tictactoe.game;
 
 public class TicTacToeGame {
+    public static char[][] inicializarTabuleiro(char[][] tabuleiro) {
+        for (int i = 0; i < tabuleiro.length; i++) {
 
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                tabuleiro[i][j] = ' ';
+            }
+        }
+        return tabuleiro;
+    }
+
+    public static void main(String[] args) {
+        char[][] tabuleiro = new char[3][3];
+
+        inicializarTabuleiro(tabuleiro);
+
+        System.out.println("Tabuleiro inicializado:");
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.print("[" + tabuleiro[i][j] + "]");
+            }
+
+            System.out.println();
+        }
+    }
 }
