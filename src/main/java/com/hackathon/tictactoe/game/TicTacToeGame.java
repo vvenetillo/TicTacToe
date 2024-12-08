c9804fa47c05c5a4c15757f98eb011e31e5205d2
Arthur <arthur.ramalho.freire@gmail.com>
2024-12-08 16:53:56 -0300

    feat(TicTacToeGame):Adiciona método teveGanhador e métodos auxiliares.

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -1,5 +1,64 @@
 package com.hackathon.tictactoe.game;
 
 public class TicTacToeGame {
+    public static boolean teveGanhador(char[][] tabuleiro, char caractereJogador) {
+        return teveGanhadorLinha(tabuleiro, caractereJogador) ||
+                teveGanhadorColuna(tabuleiro, caractereJogador) ||
+                teveGanhadorDiagonalPrincipal(tabuleiro, caractereJogador) ||
+                teveGanhadorDiagonalSecundaria(tabuleiro, caractereJogador);
+    }
+    private static boolean teveGanhadorLinha(char[][] tabuleiro, char caractereJogador) {
+        for (int i = 0; i < tabuleiro.length; i++) {
+            boolean venceu = true;
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                if (tabuleiro[i][j] != caractereJogador) {
+                    venceu = false;
+                    break;
+                }
+            }
+            if (venceu) return true;
+        }
+        return false;
+    }
+    private static boolean teveGanhadorColuna(char[][] tabuleiro, char caractereJogador) {
+        for (int j = 0; j < tabuleiro[0].length; j++) {
+            boolean venceu = true;
+            for (int i = 0; i < tabuleiro.length; i++) {
+                if (tabuleiro[i][j] != caractereJogador) {
+                    venceu = false;
+                    break;
+                }
+            }
+            if (venceu) return true;
+        }
+        return false;
+    }
+    private static boolean teveGanhadorDiagonalPrincipal(char[][] tabuleiro, char caractereJogador) {
+        for (int i = 0; i < tabuleiro.length; i++) {
+            if (tabuleiro[i][i] != caractereJogador) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    private static boolean teveGanhadorDiagonalSecundaria(char[][] tabuleiro, char caractereJogador) {
+        int n = tabuleiro.length;
+        for (int i = 0; i < n; i++) {
+            if (tabuleiro[i][n - 1 - i] != caractereJogador) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] tabuleiro = {
+                {'X', 'X', 'X'},
+                {'O', 'O', ' '},
+                {' ', 'O', ' '}
+        };
+        System.out.println("Jogador X venceu? " + teveGanhador(tabuleiro, 'X')); // true
+        System.out.println("Jogador O venceu? " + teveGanhador(tabuleiro, 'O')); // false
+    }
 }
