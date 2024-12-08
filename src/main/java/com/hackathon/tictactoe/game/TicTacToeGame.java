00870b6d2d50523ec9b867dab41e7c9bd52f8ad4
Arthur <arthur.ramalho.freire@gmail.com>
2024-12-08 16:05:41 -0300

    feat(TicTacToeGame): Adciona o método converterJogadaStringParaVetorInt na classe TicTacToeGame.

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -2,4 +2,19 @@ package com.hackathon.tictactoe.game;
 
 public class TicTacToeGame {
 
+    public static int[] converterJogadaStringParaVetorInt(String jogada) {
+        int[] vetorJogada = new int[2];
+
+        vetorJogada[0] = Character.getNumericValue(jogada.charAt(0));
+        vetorJogada[1] = Character.getNumericValue(jogada.charAt(1));
+
+        return vetorJogada;
+    }
+
+    public static void main(String[] args) {
+        String jogada = "12"; // Linha 1, Coluna 2
+        int[] vetor = converterJogadaStringParaVetorInt(jogada);
+        
+        System.out.println("Jogada convertida: [" + vetor[0] + ", " + vetor[1] + "]");
+    }
 }
