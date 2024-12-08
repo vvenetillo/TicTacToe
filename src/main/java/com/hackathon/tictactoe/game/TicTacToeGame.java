946f684cb9efc7b9bfe221b02ea7c56671af8196
lorenacgouveia <lorenaacgouveia@gmail.com>
2024-12-08 14:13:20 -0300

    add method processUserMover

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -2,4 +2,17 @@ package com.hackathon.tictactoe.game;
 
 public class TicTacToeGame {
 
+    private void processUserMove() {
+        // Obtém a jogada do usuário
+        int move = ui.getPlayerMove();  
+        
+        // Valida a jogada do usuário
+        if (moveValidator.isValidMove(move)) {
+            board.makeMove(currentPlayer, move);  // Realiza a jogada do usuário
+            ui.displayBoard(board);  // Exibe o tabuleiro após a jogada
+        } else {
+            ui.displayInvalidMove();  // Informa que a jogada é inválida
+        }
+    }
+
 }
