2f7e9bd6d669d1c4d99326f27cb215d537135140
Jorgeluisreis <ctt.jluis@gmail.com>
2024-12-08 20:21:22 -0300

    fix: Resolvido conflito de merge

--- src/main/java/com/hackathon/tictactoe/Main.java
+++ src/main/java/com/hackathon/tictactoe/Main.java
@@ -3,9 +3,9 @@ package com.hackathon.tictactoe;
 import com.hackathon.tictactoe.ui.ConsoleUI;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println(ConsoleUI.splashScreen());
-        Thread.sleep(5000);
-        ConsoleUI.exibirMenu();
-    }
+  public static void main(String[] args) throws InterruptedException {
+    System.out.println(ConsoleUI.splashScreen());
+    Thread.sleep(5000);
+    ConsoleUI.exibirMenu();
+  }
 }
--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -3,25 +3,15 @@ package com.hackathon.tictactoe.game;
 import net.jorgedev.ConsoleClear;
 
 public class TicTacToeGame {
-    public static void exibirTabuleiro(char[][] tabuleiro) {
-        // Limpa o console antes de exibir o tabuleiro
-        ConsoleClear.run();
+   public static void exibirTabuleiro(char[][] tabuleiro) {
+      ConsoleClear.run();
 
-        System.out.println("Tabuleiro:");
-        for (int i = 0; i < tabuleiro.length; i++) {
-            for (int j = 0; j < tabuleiro[i].length; j++) {
-                System.out.print("[" + tabuleiro[i][j] + "]");
-            }
-            System.out.println();
-        }
-    }
-    public static void main(String[] args) {
-        // Teste do método exibirTabuleiro
-        char[][] tabuleiro = {
-                {'X', ' ', 'O'},
-                {' ', 'O', ' '},
-                {'X', ' ', ' '}
-        };
-        exibirTabuleiro(tabuleiro);
-    }
+      System.out.println("Tabuleiro:");
+      for (int i = 0; i < tabuleiro.length; i++) {
+         for (int j = 0; j < tabuleiro[i].length; j++) {
+            System.out.print("[" + tabuleiro[i][j] + "]");
+         }
+         System.out.println();
+      }
+   }
 }
