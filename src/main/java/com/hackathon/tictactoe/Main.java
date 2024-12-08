36b828de8e0b84048636b6b42d112c901e99c9d6
Jorgeluisreis <ctt.jluis@gmail.com>
2024-12-08 20:30:49 -0300

    fix: Corrigido possíveis problemas de merge.

--- src/main/java/com/hackathon/tictactoe/Main.java
+++ src/main/java/com/hackathon/tictactoe/Main.java
@@ -1,22 +1,11 @@
 package com.hackathon.tictactoe;
 
-<<<<<<< HEAD
-import net.jorgedev.*;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Teste");
-        ConsoleClear.run();
-        System.out.println("Hello World!");
-    }
-} 
-=======
-import com.hackathon.tictactoe.game.TicTacToeGame;
 import com.hackathon.tictactoe.ui.ConsoleUI;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(ConsoleUI.splashScreen());
+        Thread.sleep(5000);
         ConsoleUI.exibirMenu();
-        TicTacToeGame.exibirVitoriaUsuario();
     }
 }
->>>>>>> 021fc07e5e79d37dbb155d63dbe34ad623238d2b
--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -4,37 +4,30 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
-    private static final char[] CARACTERES_IDENTIFICADORES_ACEITOS = {'X', 'O'};
+   private static final char[] CARACTERES_IDENTIFICADORES_ACEITOS = { 'X', 'O' };
 
-    public static char obterCaractereUsuario(Scanner teclado) {
-        char caractereUsuario;
+   public static char obterCaractereUsuario(Scanner teclado) {
+      char caractereUsuario;
 
-        while (true) {
-            System.out.print("Escolha o caractere que deseja usar (X ou O): ");
-            caractereUsuario = teclado.next().charAt(0);
+      while (true) {
+         System.out.print("Escolha o caractere que deseja usar (X ou O): ");
+         caractereUsuario = teclado.next().charAt(0);
 
-            boolean valido = false;
-            for (char c : CARACTERES_IDENTIFICADORES_ACEITOS) {
-                if (c == caractereUsuario) {
-                    valido = true;
-                    break;
-                }
+         boolean valido = false;
+         for (char c : CARACTERES_IDENTIFICADORES_ACEITOS) {
+            if (c == caractereUsuario) {
+               valido = true;
+               break;
             }
-            
-            if (valido) {
-                break;
-            } else {
-                System.out.println("Caractere inválido! Por favor, escolha apenas X ou O.");
-            }
-        }
+         }
 
-        return caractereUsuario;
-    }
+         if (valido) {
+            break;
+         } else {
+            System.out.println("Caractere inválido! Por favor, escolha apenas X ou O.");
+         }
+      }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        char caractereUsuario = obterCaractereUsuario(scanner);
-        System.out.println("Você escolheu o caractere: " + caractereUsuario);
-    }
+      return caractereUsuario;
+   }
 }
