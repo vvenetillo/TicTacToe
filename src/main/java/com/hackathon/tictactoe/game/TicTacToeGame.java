b621579468fef5e86c2d8f1b1d3dca7aaf866d28
lorenacgouveia <lorenaacgouveia@gmail.com>
2024-12-08 14:20:39 -0300

    add method processComputerMove

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -2,4 +2,21 @@ package com.hackathon.tictactoe.game;
 
 public class TicTacToeGame {
 
+    private void processComputerMove() {
+        // O computador escolhe a jogada automaticamente
+        int move = getComputerMove();  
+        board.makeMove(currentPlayer, move);  // Realiza a jogada do computador
+        ui.displayBoard(board);  // Exibe o tabuleiro após a jogada
+    }
+    
+    private int getComputerMove() {
+        // Estratégia simples: escolhe a primeira posição vazia
+        for (int i = 0; i < board.getSize(); i++) {
+            if (board.getCell(i) == ' ') {  // Verifica se a célula está vazia
+                return i;  // Retorna a primeira posição disponível
+            }
+        }
+        return -1;  // Caso não haja espaço, o que não deveria acontecer
+    }
+    
 }
