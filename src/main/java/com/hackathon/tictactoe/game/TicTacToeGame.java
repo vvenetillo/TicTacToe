636ebb970ff7b23e002adfd21108113b36f3f5d5
Arthur <arthur.ramalho.freire@gmail.com>
2024-12-08 17:07:54 -0300

    feat(TicTacToeGame):Adiciona método exibirTabuleiro.

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -1,5 +1,27 @@
 package com.hackathon.tictactoe.game;
 
-public class TicTacToeGame {
+import net.jorgedev.ConsoleClear;
 
+public class TicTacToeGame {
+    public static void exibirTabuleiro(char[][] tabuleiro) {
+        // Limpa o console antes de exibir o tabuleiro
+        ConsoleClear.run();
+
+        System.out.println("Tabuleiro:");
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.print("[" + tabuleiro[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        // Teste do método exibirTabuleiro
+        char[][] tabuleiro = {
+                {'X', ' ', 'O'},
+                {' ', 'O', ' '},
+                {'X', ' ', ' '}
+        };
+        exibirTabuleiro(tabuleiro);
+    }
 }
