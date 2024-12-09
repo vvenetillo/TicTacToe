6c999ce1bebbc76353fc9a5668ef2815a9bb1d7c
lorenacgouveia <lorenaacgouveia@gmail.com>
2024-12-09 19:30:46 -0300

    change name method

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -1,36 +1,37 @@
+package com.hackathon.tictactoe.game;
 import java.util.Scanner;
 
 public class TicTacToeGame {
 
-    private char[] board = new char[9];  // Tabuleiro com 9 espaços
-    private char currentPlayer = 'X';  // Jogador atual (X ou O)
+    private char[] tabuleiro = new char[9];  // Tabuleiro com 9 espaços
+    private char jogadorAtual = 'X';  // Jogador atual (X ou O)
     
     public TicTacToeGame() {
         for (int i = 0; i < 9; i++) {
-            board[i] = ' ';  // Inicializa o tabuleiro vazio
+            tabuleiro[i] = ' ';  // Inicializa o tabuleiro vazio
         }
     }
 
-    private void processarAVezDoUsuario() {
+    private void processarVezDoUsuario() {
         Scanner scanner = new Scanner(System.in);
-        int move;
+        int jogada;
 
-        System.out.println("Vez do jogador " + currentPlayer + ". Digite uma posição (0 a 8): ");
-        move = scanner.nextInt();  // Obtém a jogada do usuário
+        System.out.println("Vez do jogador " + jogadorAtual + ". Digite uma posição (0 a 8): ");
+        jogada = scanner.nextInt();  // Obtém a jogada do usuário
 
         // Valida a jogada (deve ser uma posição livre)
-        if (move >= 0 && move < 9 && board[move] == ' ') {
-            board[move] = currentPlayer;  // Realiza a jogada
-            displayBoard();  // Exibe o tabuleiro após a jogada
+        if (jogada >= 0 && jogada < 9 && tabuleiro[jogada] == ' ') {
+            tabuleiro[jogada] = jogadorAtual;  // Realiza a jogada
+            exibirTabuleiro();  // Exibe o tabuleiro após a jogada
         } else {
             System.out.println("Jogada inválida. Tente novamente.");
         }
     }
 
-    private void displayBoard() {
+    private void exibirTabuleiro() {
         // Exibe o tabuleiro
         for (int i = 0; i < 9; i++) {
-            System.out.print(board[i] + " ");
+            System.out.print(tabuleiro[i] + " ");
             if (i % 3 == 2) {
                 System.out.println();
             }
@@ -38,7 +39,7 @@ public class TicTacToeGame {
     }
 
     public static void main(String[] args) {
-        TicTacToeGame game = new TicTacToeGame();
-        game.processarAVezDoUsuario();  // Chama a função para o primeiro turno
+        TicTacToeGame jogo = new TicTacToeGame();
+        jogo.processarVezDoUsuario();  // Chama a função para o primeiro turno
     }
 }
