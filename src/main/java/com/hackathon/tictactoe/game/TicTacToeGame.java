f85423e0838f7ece5669d829619b49bf5fa290aa
lorenacgouveia <lorenaacgouveia@gmail.com>
2024-12-09 19:28:13 -0300

    change name method

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -2,21 +2,46 @@ package com.hackathon.tictactoe.game;
 
 public class TicTacToeGame {
 
+    private char[] tabuleiro = new char[9];  // Tabuleiro com 9 espaços
+    private char jogadorAtual = 'X';  // Jogador atual (X ou O)
+    
+    public TicTacToeGame() {
+        for (int i = 0; i < 9; i++) {
+            tabuleiro[i] = ' ';  // Inicializa o tabuleiro vazio
+        }
+    }
+
     private void processarAVezDoComputador() {
-        // O computador escolhe a jogada automaticamente
-        int move = getComputerMove();  
-        board.makeMove(currentPlayer, move);  // Realiza a jogada do computador
-        ui.displayBoard(board);  // Exibe o tabuleiro após a jogada
+        int jogada = obterJogadaComputador();  // O computador escolhe a jogada automaticamente
+        if (jogada != -1) {
+            tabuleiro[jogada] = jogadorAtual;  // Realiza a jogada do computador
+            exibirTabuleiro();  // Exibe o tabuleiro após a jogada
+        }
     }
     
-    private int getComputerMove() {
+    private int obterJogadaComputador() {
         // Estratégia simples: escolhe a primeira posição vazia
-        for (int i = 0; i < board.getSize(); i++) {
-            if (board.getCell(i) == ' ') {  // Verifica se a célula está vazia
+        for (int i = 0; i < 9; i++) {
+            if (tabuleiro[i] == ' ') {  // Verifica se a célula está vazia
                 return i;  // Retorna a primeira posição disponível
             }
         }
         return -1;  // Caso não haja espaço, o que não deveria acontecer
     }
-    
+
+    private void exibirTabuleiro() {
+        // Exibe o tabuleiro
+        for (int i = 0; i < 9; i++) {
+            System.out.print(tabuleiro[i] + " ");
+            if (i % 3 == 2) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TicTacToeGame jogo = new TicTacToeGame();
+        jogo.processarAVezDoComputador();  // Chama a função para o primeiro turno do computador
+    }
 }
+
