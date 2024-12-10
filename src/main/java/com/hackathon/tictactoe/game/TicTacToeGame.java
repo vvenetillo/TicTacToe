84bc0cc97e245e5965e3341d2cc03f86a8990d82
Gilvan <gilvanribeiro773@gmail.com>
2024-12-10 19:15:17 -0300

    jogadaValida

--- src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
+++ src/main/java/com/hackathon/tictactoe/game/TicTacToeGame.java
@@ -1,15 +1,8 @@
 package com.hackathon.tictactoe.game;
 
+
 public class TicTacToeGame {
-    
-      public  static void exibirVitoriaUsuario() {
-         System.out.println("O jogador venceu!"); 
-        
-          System.out.println(" _____");
-         System.out.println(" | ^ ^ |"); 
-            System.out.println(" / \\");
-         System.out.println(" \\ U /"); 
-          System.out.println(" |||||"); 
-         
-           }
-}
+    static boolean jogadaValida(String posicoesLivres, int linha, int coluna) { 
+        String posicao = linha + "," + coluna; 
+        return posicoesLivres.contains(posicao); }
+    }
